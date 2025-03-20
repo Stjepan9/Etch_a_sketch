5408190b3c639e2f0fc3aa0e19bea4f11520cf34
Stjepan Svib <svib.stjepan@gmail.com>
2025-03-20 15:09:07 +0100

    Added input(color), colorBtn, eraser, rainbow btns

--- script.js
+++ script.js
@@ -1,52 +1,25 @@
-//PSEUDOCODE
-
-// CREATE A HEADER FOR ETCH_A_SKETCH
-// Create button container
-// Create buttons inside container(color, rainbow, eraser, gradient)
-// Create Input-color above buttons
-// Create a div container that will contain grid ( small cubes inside big container)
-
-
-//PSEUDOCODE JAVASCRIPT
-
-//Select all buttons and elements using QuerySelector
-//Select grid container
-//Select buttons (Color, Rainbow, Clear, Eraser)
-//Select color picker
-//Select grid size slider and display value
-
-//Create a function to generate the grid
-//Loop to create (gridSize x gridSize) div elements ( use that lesson x and y that shows coordinates)
-//Apply CSS grid styling for uniform alignment
-//Add event listeners for mouse events (drawing)
-
-//Function to handle color selection (COLOR BUTTON)
-//When "Color" button is clicked, apply selected color
-
-//Function for Rainbow Mode (RAINBOW BUTTON)
-//When "Rainbow" is clicked, assign random color on hover
-
-//Function to clear the grid (CLEAR BUTTON)
-//When "Clear" is clicked, reset all grid cells to default color
-
-//Function for Eraser  (ERASER)
-//When "Eraser" is clicked, set drawing color to background color
-
-//Function to update grid size
-//Clear the existing grid
-//Generate a new grid based on selected size (1x1 to 64x64)
-
-//Event listeners for button clicks and input changes
-//Listen for color picker change
-//Listen for grid size slider change
-//Listen for drawing interactions (mousedown, mouseover, mouseup)
-
-
 document.addEventListener("DOMContentLoaded", function(){
     createGrid(16);
 });
 
+
+// ----- QuerySelector all the buttons -----
+// ----- DOM manipulate them so they have same width and height-----
+const colorBtn = document.querySelector("#color");
+const colorPicker = document.querySelector("#colorPicker");
+const rainbowBtn = document.querySelector("#rainbow");
+const clearBtn = document.querySelector("#clear");
+const eraserBtn = document.querySelector("#eraser");
 let gridContainer = document.querySelector("#grid-container");
+
+const buttons = document.querySelectorAll("#controls button");
+buttons.forEach((button)=>{
+    button.style.width = "150px";
+    button.style.height = "50px";
+});
+
+
+let selectedMode = "color";
 
 const createGrid = function(size){
     //------Clears grid------
@@ -59,7 +32,7 @@ const createGrid = function(size){
     gridContainer.style.width = "600px";
     gridContainer.style.height = "600px";
     gridContainer.style.display = "grid";
-    
+    gridContainer.style.border = " 10px solid #e5ece9"
     gridContainer.style.borderRadius = "30px";
 
     //----- a square is created------
@@ -70,7 +43,7 @@ const createGrid = function(size){
     // ------- Appended child to parent-------
     for(let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
-        div.style.border = `0.5px solid black`;
+        div.style.border = `0.5px solid #e5ece9`;
         div.style.width = "100%";
         div.style.height = "100%";
 
@@ -78,16 +51,22 @@ const createGrid = function(size){
 
         //---- When mouse enters div it changes color to black(default)-----
         div.addEventListener("mouseover", function(){
-            div.style.backgroundColor = "black";
+            if(selectedMode === "eraser"){
+                div.style.backgroundColor = "#e5ece9";
+            }
+            else if(selectedMode === "colorPicker"){
+                div.style.backgroundColor = colorPicker.value;
+            }
+            else if(selectedMode === "rainbow"){
+                let randomColor = `#` + Math.floor(Math.random()*16777215).toString(16).padStart(6,"0");
+                div.style.backgroundColor = randomColor;
+            }
+            else if(selectedMode === "color"){
+                div.style.backgroundColor = "black";
+            };
         });
-
-        eraserBtn.addEventListener("click", function(){
-            div.addEventListener("mouseover", function(){
-                div.style.backgroundColor = "white";
-            })
-        });
-        
     };
+
 
 };
 
@@ -106,51 +85,26 @@ inputRange.addEventListener("input",function(){
 })
 
 
-
-// ----- QuerySelector all the buttons -----
-// ----- DOM manipulate them so they have same width and height-----
-const colorBtn = document.querySelector("#color");
-const colorPicker = document.querySelector("#colorPicker");
-const rainbowBtn = document.querySelector("#rainbow");
-const clearBtn = document.querySelector("#clear");
-const eraserBtn = document.querySelector("#eraser");
-
-const buttons = document.querySelectorAll("#controls button");
-buttons.forEach((button)=>{
-    button.style.width = "150px";
-    button.style.height = "50px";
-});
-
 //---- Added functions and eventlisteners for buttons------
-let selectedColor = "black";
-const color = function(){
-
-};
-
-const rainbow = function(){
-
-};
-
-const eraser = function(){
-
-};
-
-const clear = function(){
-
-};
-
-
-colorBtn.addEventListener("click", color);
 colorPicker.addEventListener("input", function(){
-    selectedColor = this.value;
+    selectedMode = "colorPicker";
 });
-rainbowBtn.addEventListener("click", rainbow);
-eraserBtn.addEventListener("click", eraser);
-clearBtn.addEventListener("click",clear);
 
-gridContainer.addEventListener("mousemove", function(){
-
+colorBtn.addEventListener("click", function(){
+    selectedMode = "color";
 });
+
+rainbowBtn.addEventListener("click", function(){
+    selectedMode = "rainbow";
+});
+
+eraserBtn.addEventListener("click", function(){
+    selectedMode = "eraser";
+});
+
+
+
+
 
 
 
